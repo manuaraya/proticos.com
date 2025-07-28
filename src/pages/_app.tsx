e7c176beabd9cf6af19521d20dd27eeed0c19515
manuaraya <manuelaraya_vega@hotmail.com>
2025-07-27 21:00:42 -0600

    Fixing Providers routing

---
+++ src/pages/_app.tsx
@@ -0,0 +1,7 @@
+import '../styles/globals.css'; // Adjust if your CSS file path is different
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
